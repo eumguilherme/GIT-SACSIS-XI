ba2e7ef949daa42a9fb14286b303cbae57212998
eumguilherme <eumguilherme@outlook.com>
2019-06-13 20:30:26 -0300

    adicionando funcao de dividir, corrigido

--- projeto.c
+++ projeto.c
@@ -14,10 +14,16 @@ int multiplicacao(int n1, int n2){
 	return n1*n2;
 }
 
+int divisao(int n1, int n2){
+	
+	return n1/n2;
+}
+
 int main(){
 	printf("\nCalculadora!");
 	printf("\nSoma de dois numeros: %d",soma(5,5));
 	printf("\nSubtracao de dois numeros: %d",subtrai(5,2));
 	printf("\nMultiplicacao de dois numeros: %d", multiplicacao(5,3));
+	printf("\nDivisao de dois numeros: %d", divisao(8,2));
 	return 0;
 }
