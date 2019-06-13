91b41d363844ccafe028a89abe898905b2426a77
eumguilherme <eumguilherme@outlook.com>
2019-06-13 19:57:47 -0300

    adicionando funcao de subtração

--- projeto.c
+++ projeto.c
@@ -3,6 +3,14 @@ int soma(n1,n2){
 	
 	return n1+n2;
 }
+
+int subtrai(n1,n2){
+	
+	return n1-n2;
+}
 int main(){
-	printf("Soma de dois numeros: %d",soma(5,5));
+	printf("\nCalculadora!");
+	printf("\nSoma de dois numeros: %d",soma(5,5));
+	printf("\nSubtracao de dois numeros: %d",subtrai(5,2));
+	return 0;
 }
